0f0e2fda527134a935ac4a033805143a171165a2
Tyler Lederer <tclederer@gmail.com>
2018-03-14 15:44:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  circleMoving() {
+    console.log("This is Working");
+  }
+  render() {
+    var interval;
+    return (
+      <svg width={window.innerWidth} height={window.innerHeight}>
+        <rect
+          width="100%"
+          height="100%"
+          fill="none"
+          stroke="black"
+          strokeWidth="10"
+        />
+        <circle
+          onMouseDown={() => setInterval(this.circleMoving, 16.667)}
+          onMouseUp={() => clearInterval(this.circleMoving)}
+          cx="50"
+          cy="50"
+          r="40"
+          stroke="blue"
+          strokeWidth="10"
+          fill="white"
+        />
+      </svg>
+    );
+  }
+}
+
+export default App;
